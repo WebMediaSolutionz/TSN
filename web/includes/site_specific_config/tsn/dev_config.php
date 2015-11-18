88fcb6112a82e288cf667747549df221455fc23b
maxime pierre <max@webmediasolutionz.com>
2015-11-17 22:28:44 -0500

    restructuing + changed the init function of the action class

---
+++ web/includes/site_specific_config/tsn/dev_config.php
@@ -0,0 +1,15 @@
+<?php
+	defined( 'DB_DOMAIN' ) 		? null : define( 'DB_DOMAIN', 	'localhost' );
+	defined( 'DB_USERNAME' ) 	? null : define( 'DB_USERNAME', 'root' ); 
+	defined( 'DB_PASSWORD' ) 	? null : define( 'DB_PASSWORD', '' ); 
+	defined( 'DB_DATABASE' ) 	? null : define( 'DB_DATABASE', 'TSN2' ); 
+	defined( 'SITE_EMAIL' ) 	? null : define( 'SITE_EMAIL', 'info@webmediasolutionz.com' ); 
+	defined( 'SUPPORT_EMAIL' ) 	? null : define( 'SUPPORT_EMAIL', 'support@webmediasolutionz.com' ); 
+
+	defined( 'MAIL_SERVER' ) 	? null : define( 'MAIL_SERVER', 'mail.webmediasolutionz.com' ); 
+	defined( 'MAIL_PORT' ) 		? null : define( 'MAIL_PORT', '26' ); 
+	defined( 'MAIL_USERNAME' ) 	? null : define( 'MAIL_USERNAME', 'info@webmediasolutionz.com' ); 
+	defined( 'MAIL_PASSWORD' ) 	? null : define( 'MAIL_PASSWORD', 'i&p{8S%VI&P}' ); 
+
+	defined( 'DOMAIN' ) 		? null : define( 'DOMAIN', 	'localhost/~mpierre1/TSN2/public' );
+?>
