d2174e26b242311696b27243720f9c615a738c92
Maxime Pierre <maxime.pierre@telus.com>
2016-06-09 01:09:05 -0400

    [FP]: creating UFC fight pass theme

---
+++ web/public/views/fp/partials/footer.tpl.php
@@ -0,0 +1,21 @@
+			</div>
+
+			<div id="footer">
+				
+			</div>
+		</div>
+
+		<div class="container margin-bottom">
+			<div class="footer">
+				<span class="italics">&copy; <?php echo date( "Y", time() ) . " " . strtoupper( SITE_NAME ) . " " . str_replace( '*company*', PARENT_COMPANY, str_replace( '*company_website*', PARENT_COMPANY_WEBSITE, $lang[ 'powered_by' ] ) ); ?></span>
+			</div>
+		</div>
+
+		<script type="text/javascript" src="views/<?php echo $theme; ?>/scripts/javascript.js"></script>
+    </body>
+</html>
+<?php
+	if ( isset( $DB ) ) {
+		$DB->close_connection();
+	}
+?>
