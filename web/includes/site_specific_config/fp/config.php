51a4b40fe30e31b641ef7ee8898e1fd31d968ea9
Maxime Pierre <maxime.pierre@telus.com>
2016-06-09 19:55:52 -0400

    [FP]: fixing config for 'fight pass' theme

--- web/includes/site_specific_config/fp/config.php
+++ web/includes/site_specific_config/fp/config.php
@@ -1,6 +1,6 @@
 <?php
 	defined( 'SITE_NAME' ) ? null : define( 'SITE_NAME', 'UFC Fight Pass' );
-	defined( 'DEFAULT_THEME' ) ? null : define( 'DEFAULT_THEME', 'ma' );
+	defined( 'DEFAULT_THEME' ) ? null : define( 'DEFAULT_THEME', 'fp' );
 	defined( 'PROFILE_USER' ) ? null : define( 'PROFILE_USER', 89 );
 	defined( 'AUTHENTICATION_REQUIRED' ) ? null : define( 'AUTHENTICATION_REQUIRED', true );
 
