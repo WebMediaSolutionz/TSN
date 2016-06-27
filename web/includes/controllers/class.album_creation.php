e46b93134cb453891011f465bce39833b0e349fc
Maxime Pierre <maxime.pierre@telus.com>
2016-06-27 11:44:17 -0400

    [bugfixes]: fixing the 'add_pictures' and 'album_creation' controllers

--- web/includes/controllers/class.album_creation.php
+++ web/includes/controllers/class.album_creation.php
@@ -4,6 +4,10 @@
 			global $session, $lang, $page_title;
 
 			$theme = static::$theme;
+			
+			$current_page = static::$current_page;
+			$current_page_short = static::$current_page_short;
+			
 			$current_user = User::find_by_id( $session->user_id );
 			
 			include_once( static::load_template() );
