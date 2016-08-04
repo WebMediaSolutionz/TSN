4ea9656f0ee9d8a952e9113f5029856dcd96bdd6
maxime pierre <max@webmediasolutionz.com>
2016-08-04 08:55:28 -0400

    [sampler]: adding some models. they're not really used in the current theme but never the less, they are dependencies. this needs to be tidies up

---
+++ web/includes/model/class.comments.php
@@ -0,0 +1,94 @@
+<?php
+
+	class Comments extends Item {
+		protected static $table_name = "comments";
+		public static $db_fields = array(
+			'id'			=> 		'auto-increment',
+			'value' 		=> 		'string',
+			'user_id' 		=> 		'int',
+			'post_id' 		=> 		'int', 
+			'picture_id' 	=> 		'int',
+			'album_id' 		=> 		'int',
+			'video_id' 		=> 		'int',
+			'track_id' 		=> 		'int',
+			'date' 			=> 		'datetime'
+			);
+		public $id;
+		public $value;
+		public $user_id;
+		public $post_id;
+		public $picture_id;
+		public $album_id;
+		public $video_id;
+		public $track_id;
+		public $date;
+		public $you_like;
+
+		public static function get_comments_for_item ( $item ) {
+			global $DB;
+
+			$item_type = strtolower( get_class( $item ) );
+
+			switch ( $item_type ) {
+				case 'post' 	:	$sql = "SELECT * FROM " . static::$table_name . " WHERE post_id = {$item->id} ORDER BY date";
+									break;
+
+				case 'picture' 	:	$sql = "SELECT * FROM " . static::$table_name . " WHERE picture_id = {$item->id} ORDER BY date";
+									break;
+
+				case 'album' 	:	$sql = "SELECT * FROM " . static::$table_name . " WHERE album_id = {$item->id} ORDER BY date";
+									break;
+
+				case 'video' 	:	$sql = "SELECT * FROM " . static::$table_name . " WHERE video_id = {$item->id} ORDER BY date";
+									break;
+
+				case 'track' 	:	$sql = "SELECT * FROM " . static::$table_name . " WHERE track_id = {$item->id} ORDER BY date";
+									break;
+			}
+
+			return self::find_by_sql( $sql );
+		}
+
+		public static function delete_comments_on_post ( $post_id ) {
+			global $DB;
+
+			$sql = "DELETE FROM " . static::$table_name . " WHERE post_id = {$post_id}";
+
+			$DB->query( $sql );
+			
+			return ( $DB->affected_rows() == 1 );
+		}
+
+		public static function commenters ( $item, $except_for_this_user_id = null ) {
+			global $DB, $session;
+
+			$item_type = strtolower( get_class( $item ) );
+
+			$users = array();
+
+			if ( $item_type != "comments" ) {
+				switch ( $item_type ) {
+					case 'comments'		:		$item_type = 'comment';
+												break;
+				}
+
+				$sql = "SELECT user_id FROM " . static::$table_name . " ";
+				$sql .= "WHERE {$item_type}_id = {$item->id} ";
+
+				if ( $except_for_this_user_id != null ) {
+					$sql .= "AND user_id != {$except_for_this_user_id}";
+				}
+
+				$result_set = $DB->query( $sql );
+
+				while ( $row = $DB->fetch_array( $result_set ) ) {
+					$object = static::instantiate( $row );
+					$users[] = User::find_by_id( $object->user_id );
+				}
+			}
+
+			return $users;
+		}
+	}
+
+?>
---
+++ web/includes/model/class.likes.php
@@ -0,0 +1,100 @@
+<?php
+
+	class Likes extends DatabaseObject {
+		protected static $table_name = "likes";
+		public static $db_fields = array(
+			'id'			=> 		'auto-increment',
+			'user_id' 		=> 		'int',
+			'post_id' 		=> 		'int',
+			'comment_id' 	=> 		'int',
+			'picture_id' 	=> 		'int',
+			'album_id' 		=> 		'int',
+			'video_id' 		=> 		'int',
+			'track_id' 		=> 		'int',
+			'date' 			=> 		'datetime'
+			);
+		public $id;
+		public $user_id;
+		public $post_id;
+		public $comment_id;
+		public $picture_id;
+		public $album_id;
+		public $video_id;
+		public $track_id;
+		public $date;
+
+		public static function unlike ( $user_id, $item ) {
+			global $DB;
+
+			$item_type = strtolower( get_class( $item ) );
+
+			switch ( $item_type ) {
+				case 'comments'		:		$item_type = 'comment';
+											break;
+			}
+
+			$sql = "DELETE FROM " . static::$table_name . " ";
+			$sql .= "WHERE user_id=" . $DB->escape_value( $user_id ) . " ";
+			$sql .= "AND {$item_type}_id=" . $DB->escape_value( $item->id ) . " ";
+			$sql .= "LIMIT 1";
+
+			$DB->query( $sql );
+			
+			return ( $DB->affected_rows() == 1 );
+		}
+
+		public static function you_like ( $user_id, $item ) {
+			global $DB, $session;
+
+			$item_type = strtolower( get_class( $item ) );
+
+			switch ( $item_type ) {
+				case 'comments'		:		$item_type = 'comment';
+											break;
+			}
+
+			$sql = "SELECT * FROM " . static::$table_name . " ";
+			$sql .= "WHERE user_id = {$user_id} ";
+			$sql .= "AND {$item_type}_id = {$item->id} ";
+			$sql .= "LIMIT 1";
+
+			$result_set = $DB->query( $sql );
+
+			$object_array = array();
+
+			while ( $row = $DB->fetch_array( $result_set ) ) {
+				$object_array[] = static::instantiate( $row );
+			}
+			return ( count( $object_array ) === 1 );
+		}
+
+		public static function likers ( $item, $except_for_this_user_id = null ) {
+			global $DB, $session;
+
+			$item_type = strtolower( get_class( $item ) );
+
+			switch ( $item_type ) {
+				case 'comments'		:		$item_type = 'comment';
+											break;
+			}
+
+			$sql = "SELECT user_id FROM " . static::$table_name . " ";
+			$sql .= "WHERE {$item_type}_id = {$item->id} ";
+
+			if ( $except_for_this_user_id != null ) {
+				$sql .= "AND user_id != {$except_for_this_user_id}";
+			}
+
+			$result_set = $DB->query( $sql );
+
+			$users = array();
+
+			while ( $row = $DB->fetch_array( $result_set ) ) {
+				$object = static::instantiate( $row );
+				$users[] = User::find_by_id( $object->user_id );
+			}
+			return $users;
+		}
+	}
+
+?>
