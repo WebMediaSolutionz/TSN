bee778645d57a9b1b28fa082b2772853a82c7612
maxime pierre <max@webmediasolutionz.com>
2017-01-13 17:49:35 -0500

    dashboard

---
+++ web/includes/controllers/class.dashboard.php
@@ -0,0 +1,22 @@
+<?php
+	class DashboardCtrl extends ActionCtrl {
+		public static function load () {
+			global $session, $lang, $page_title, $current_page;
+
+			$theme = static::$theme;
+
+			$current_page = static::$current_page;
+			$current_page_short = static::$current_page_short;
+
+			if ( defined( 'PROFILE_USER' ) ) {
+				$profile_user = User::find_by_id( PROFILE_USER );
+			}
+
+			if ( isset( $session->user_id ) ) {
+				$current_user = User::find_by_id( $session->user_id );
+			}
+
+			include_once( static::load_template() );
+		}
+	}
+?>
---
+++ web/public/views/kn/authenticated/dashboard.tpl.php
@@ -0,0 +1,9 @@
+<?php include( 'partials/header.tpl.php' ); ?>
+
+<div class="content">
+	<h1>DASHBOARD</h1>
+
+	<div class="clearfix"></div>
+</div>
+
+<?php include( 'partials/footer.tpl.php' ); ?>
