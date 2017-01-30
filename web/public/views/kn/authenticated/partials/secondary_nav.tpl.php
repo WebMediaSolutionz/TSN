a3afe2d4d650a04e886d8d47caa813a1d123d492
maxime pierre <max@webmediasolutionz.com>
2017-01-29 22:34:42 -0500

    adding highlight on the 'dashboard' and 'settings' links in the nav when they're the current page

--- web/public/views/kn/authenticated/partials/secondary_nav.tpl.php
+++ web/public/views/kn/authenticated/partials/secondary_nav.tpl.php
@@ -6,10 +6,10 @@
         <a href="#"><span>conversation</span></a>
     </li>
     <li class="left">
-        <a href="dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i></a>
+        <a class="<?php echo ( $current_page_short === 'dashboard' ) ? 'current' : ''; ?>" href="dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i></a>
     </li>
     <li class="left">
-        <a href="settings.php"><i class="fa fa-cog" aria-hidden="true"></i></a>
+        <a class="<?php echo ( $current_page_short === 'settings' ) ? 'current' : ''; ?>" href="settings.php"><i class="fa fa-cog" aria-hidden="true"></i></a>
     </li>
     <li class="left hide">
         <a href="login.php?action=logout"><span>logout</span></a>
