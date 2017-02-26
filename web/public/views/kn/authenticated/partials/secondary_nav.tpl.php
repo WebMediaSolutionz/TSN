c3aa9dd6a082f9b28054bff45aaadb8d9d8e0962
maxime pierre <max@webmediasolutionz.com>
2017-02-26 18:26:35 -0500

    permissions in the kylee nash theme

--- web/public/views/kn/authenticated/partials/secondary_nav.tpl.php
+++ web/public/views/kn/authenticated/partials/secondary_nav.tpl.php
@@ -5,9 +5,11 @@
     <li class="left hide">
         <a href="#"><span>conversation</span></a>
     </li>
-    <li class="left">
-        <a class="<?php echo ( $current_page_short === 'dashboard' ) ? 'current' : ''; ?>" href="dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i></a>
-    </li>
+    <?php if ( $profile_user->id === $current_user->id ) { ?>
+        <li class="left">
+            <a class="<?php echo ( $current_page_short === 'dashboard' ) ? 'current' : ''; ?>" href="dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i></a>
+        </li>
+    <?php } ?>
     <li class="left">
         <a class="<?php echo ( $current_page_short === 'settings' ) ? 'current' : ''; ?>" href="settings.php"><i class="fa fa-cog" aria-hidden="true"></i></a>
     </li>
