dcf0a40c564db661d6798c3dc4101e5005131c36
maxime pierre <max@webmediasolutionz.com>
2017-05-12 00:54:00 -0400

    [KN-5]: fix UI throughout site

--- web/public/views/kn/authenticated/partials/post_type_3.tpl.php
+++ web/public/views/kn/authenticated/partials/post_type_3.tpl.php
@@ -9,17 +9,15 @@
 <div class="single_post post_type_<?php echo $post->post_type; ?>">
 	<?php include( "post_top_part.tpl.php" ); ?>
 
-		<br />
-
-		<span><?php echo $post->value; ?></span><br /><br />
-		
-		<br />
-		
-		<div class="post_operations actions">
-			<span>
-				<a class="js-action js-like" href="<?php echo $post->you_like ? static::$action_unlike_link : static::$action_like_link; ?>&post_id=<?php echo $post->id; ?>"><?php echo $post->you_like ? $lang[ 'unlike' ] : $lang[ 'like' ]; ?></a> &middot; <a class="js-action js-comment" href="#">Comment</a><!-- &middot; <a href="share.php?post_id=<?php echo $post->id; ?>"><?php echo $lang[ 'share' ]; ?></a> --><span class="js-nb_likes"><?php echo count( $post->get_likers() ) === 1 ? ' &middot; 1 like' : ( count( $post->get_likers() ) !== 0 ? ' &middot; ' . count( $post->get_likers() ) . ' likes': '' );?></span><?php echo count( $post->get_commenters() ) === 1 ? ' &middot; 1 comment' : ( count( $post->get_commenters() ) !== 0 ? ' &middot; ' . count( $post->get_commenters() ) . ' comments': '' );?>
-			</span>
+		<div class="post_content">
+			<span><?php echo $post->value; ?></span>
 		</div>
+		
+		<?php if ($session->is_logged_in()) { ?>
+			<div class="post_operations actions">
+				<span><a class="js-action js-like" href="<?php echo $post->you_like ? static::$action_unlike_link : static::$action_like_link; ?>&post_id=<?php echo $post->id; ?>"><?php echo $post->you_like ? $lang[ 'unlike' ] : $lang[ 'like' ]; ?></a> &middot; <a class="js-action js-comment" href="#">Comment</a><!-- &middot; <a href="share.php?post_id=<?php echo $post->id; ?>"><?php echo $lang[ 'share' ]; ?></a> --><span class="js-nb_likes"><?php echo count( $post->get_likers() ) === 1 ? ' &middot; 1 like' : ( count( $post->get_likers() ) !== 0 ? ' &middot; ' . count( $post->get_likers() ) . ' likes': '' );?></span><?php echo count( $post->get_commenters() ) === 1 ? ' &middot; 1 comment' : ( count( $post->get_commenters() ) !== 0 ? ' &middot; ' . count( $post->get_commenters() ) . ' comments': '' );?></span>
+			</div>
+		<?php } ?>
 	</div>
 	<?php if ( $author->id === $current_user->id ) { ?>
 		<div class="post_actions right">
@@ -34,19 +32,21 @@
 			<?php include( "comment.tpl.php" ); ?>
 		<?php } ?>
 
-		<div class="comment_posting_section">
-			<form action="<?php echo static::$action_comment_link; ?>" method="post">
-				<a id="profile_pic_thumbnail" class="left block" href="profile.php?profile_id=<?php echo $author->id; ?>">
-					<img src="<?php echo $current_user_img; ?>" />
-				</a>
-				<input class="left js-input_comment" type="text" placeholder="Write a comment..." name="value" />
-				<input type="hidden" name="post_id" value="<?php echo $post->id; ?>" />
-				<input type="hidden" name="current_user_id" value="<?php echo $current_user->id; ?>" />
-				<input type="hidden" name="current_user_fullname" value="<?php echo $current_user->full_name(); ?>" />
-				<input class="left js-submit_comment" type="submit" name="submit" value="comment" />
-				<div class="clear"></div>
-			</form>
-		</div>
+		<?php if ($session->is_logged_in()) { ?>
+			<div class="comment_posting_section">
+				<form action="<?php echo static::$action_comment_link; ?>" method="post">
+					<a id="profile_pic_thumbnail" class="left block" href="profile.php?profile_id=<?php echo $author->id; ?>">
+						<img src="<?php echo $current_user_img; ?>" />
+					</a>
+					<input class="right js-input_comment" type="text" placeholder="Write a comment..." name="value" />
+					<input type="hidden" name="post_id" value="<?php echo $post->id; ?>" />
+					<input type="hidden" name="current_user_id" value="<?php echo $current_user->id; ?>" />
+					<input type="hidden" name="current_user_fullname" value="<?php echo $current_user->full_name(); ?>" />
+					<input class="left js-submit_comment" type="submit" name="submit" value="comment" />
+					<div class="clear"></div>
+				</form>
+			</div>
+		<?php } ?>
 	</div>
 	<div class="clear"></div>        
 </div>
--- web/public/views/kn/unauthenticated/partials/newsfeed.tpl.php
+++ web/public/views/kn/unauthenticated/partials/newsfeed.tpl.php
@@ -1,21 +1,17 @@
 <?php if ( $session->is_logged_in() && $current_user->id === $profile_user->id ) { ?>
 	<div class="status_textarea">
 		<form action="blog.php?action=post_to_wall" method="post">
-			<textarea id="status_updater" name="value"></textarea>
+			<textarea id="status_updater" name="value" placeholder="Write a post..."></textarea>
 
 			<input type="hidden" name="current_user_fullname" value="<?php echo $current_user->full_name(); ?>" />
 			<input type="hidden" name="wall_id" value="<?php echo $current_user->id; ?>" />
 
-			<br />
-
 			<input class="js-submit_post" id="post_to_wall" type="submit" name="submit" value="<?php echo $lang[ 'post_to_wall' ]; ?>" />
 		</form>
 	</div>
-
-	<br><br>
 <?php } ?>
 
-<div id="wall" class="left">
+<div id="wall">
 	<?php foreach( $posts as $post ) { ?>
 		<?php include( "post_type_{$post->post_type}.tpl.php" ); ?>
 	<?php } ?>
