043b74e30b9a7a24de8e1526987f3fd5916e857d
maxime pierre <mpierre@pulseinc.com>
2017-05-17 00:16:10 -0400

    not showing empty 'latest' sections

--- web/public/views/kn/authenticated/home.tpl.php
+++ web/public/views/kn/authenticated/home.tpl.php
@@ -1,32 +1,38 @@
 <?php include( 'partials/header.tpl.php' ); ?>
 
 <div class="content">
-	<h1>LATEST VIDEOS</h1>
+	<?php if ( count( $videos ) > 0 ) { ?>
+		<h1>LATEST VIDEOS</h1>
 
-	<div class="album_gallery">
-		<?php foreach ( $videos as $video ) { ?>
-			<?php include( 'partials/video_thumb.tpl.php' ); ?>
-		<?php } ?>
-	</div>
+		<div class="album_gallery">
+			<?php foreach ( $videos as $video ) { ?>
+				<?php include( 'partials/video_thumb.tpl.php' ); ?>
+			<?php } ?>
+		</div>
 
-	<div class="clearfix"></div>
-
-	<h1>LATEST PHOTO SETS</h1>
-
-	<div class="album_gallery">
-		<?php
-			foreach ( $albums as $album ) {
-				include( 'partials/album_thumbnail.tpl.php' );
-			}
-		?>
 		<div class="clearfix"></div>
-	</div>
+	<?php } ?>
 
-	<h1>LATEST BLOG POSTS</h1>
+	<?php if ( count( $albums ) > 0 ) { ?>
+		<h1>LATEST PHOTO SETS</h1>
 
-	<?php include( 'partials/newsfeed.tpl.php' ); ?>
+		<div class="album_gallery">
+			<?php
+				foreach ( $albums as $album ) {
+					include( 'partials/album_thumbnail.tpl.php' );
+				}
+			?>
+			<div class="clearfix"></div>
+		</div>
+	<?php } ?>
 
-	<div class="clearfix"></div>
+	<?php if ( count( $posts ) > 0 ) { ?>
+		<h1>LATEST BLOG POSTS</h1>
+
+		<?php include( 'partials/newsfeed.tpl.php' ); ?>
+
+		<div class="clearfix"></div>
+	<?php } ?>
 </div>
 
 <?php include( 'partials/footer.tpl.php' ); ?>
